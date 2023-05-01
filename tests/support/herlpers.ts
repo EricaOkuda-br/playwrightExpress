614a1c63472ee1e59c8b37ddfb016485730d6e47
EricaOkuda-br <ericaocuda.br@gmail.com>
2023-05-01 16:44:32 -0400

    criando o env

--- tests/support/herlpers.ts
+++ tests/support/herlpers.ts
@@ -1,11 +1,14 @@
 import { expect, APIRequestContext} from '@playwright/test'
 import { TaskModel } from '../fixtures/task.model'
+require('dotenv').config()
+
+const baseAPI = process.env.BASE_API
 
 export async function deleteTaskByHelper(request: APIRequestContext, taskName: string) {
-    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+    await request.delete(`${baseAPI}/helper/tasks/${taskName}`)
 }
 
 export async function postTak(request: APIRequestContext, task: TaskModel) {
-    const newTask = await request.post('http://localhost:3333/tasks', { data: task })
+    const newTask = await request.post(`${baseAPI}/tasks`, { data: task })
     expect(newTask.ok()).toBeTruthy() 
 }
